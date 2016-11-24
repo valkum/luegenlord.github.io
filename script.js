3e3ff4b2d5c2bb4dea1fa4fd6bbff2755c2d963f
luegenlord <cyborgryna@gmail.com>
2016-11-25 00:32:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout(function () {
+  responsiveVoice.speak("Marcell ist der Luegenlord! LUELUELUE!");
+}, 2000);
+setInterval(function(){ 
+  responsiveVoice.speak("Shame! Ding Ding Ding!");
+}, 8000);
