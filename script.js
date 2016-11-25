814ead8f8dc6949478f3565880c9d1825e7d3d9a
Rudi Floren <rudi.floren@googlemail.com>
2016-11-25 03:49:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setTimeout(function () {
-  responsiveVoice.speak("Marcell ist der Luegenlord! LUELUELUE!");
+  responsiveVoice.speak("Marcell is der Lügenlord! lululu!");
 }, 2000);
 setInterval(function(){ 
-  responsiveVoice.speak("Shame! Ding Ding Ding!");
+  responsiveVoice.speak("Shame! Ding Ding Ding! Boom Boom!");
 }, 8000);
